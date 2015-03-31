6111ba5981437bdcc85fc10fdf6787717549f19e
David <dmontz@gmail.com>
2015-03-31 17:07:18 -0600

    getting started

---
+++ CardView.swift
@@ -0,0 +1,58 @@
+//
+//  CardView.swift
+//  BitDate
+//
+//  Created by Jamie Montz on 3/31/15.
+//  Copyright (c) 2015 David Montz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CardView: UIView {
+    
+    private let imageView: UIImageView = UIImageView()
+    private let nameLabel: UILabel = UILabel()
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    override init() {
+        super.init()
+        initialize()
+    }
+    
+    private func initialize () {
+        
+        imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        imageView.backgroundColor = UIColor.redColor()
+        addSubview(imageView)
+        
+        nameLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        setTranslatesAutoresizingMaskIntoConstraints(false)
+        addSubview(nameLabel)
+        
+        backgroundColor = UIColor.whiteColor()
+        layer.borderWidth = 0.5
+        layer.borderColor = UIColor.lightGrayColor().CGColor
+        layer.cornerRadius = 5
+        layer.masksToBounds = true
+    }
+    
+    private func setConstraints() {
+        
+        addConstraint(NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Width, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Height, multiplier: 1.0, constant: 0))
+    }
+    
+}
