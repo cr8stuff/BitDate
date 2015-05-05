b3eacf826b1b04cb4d47336fad550d8a972bc3e4
David <dmontz@gmail.com>
2015-05-05 06:55:54 -0600

    added match queries and views

---
+++ BitDate/Match.swift
@@ -0,0 +1,9 @@
+//
+//  Match.swift
+//  BitDate
+//
+//  Created by Jamie Montz on 5/5/15.
+//  Copyright (c) 2015 David Montz. All rights reserved.
+//
+
+import Foundation
---
+++ UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  BitDate
+//
+//  Created by Jamie Montz on 5/5/15.
+//  Copyright (c) 2015 David Montz. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
